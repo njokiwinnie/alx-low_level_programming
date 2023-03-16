b068807e67839d7d37632496324a86c13597d51d
njokiwinnie <winnleykim@gmail.com>
2023-03-16 14:46:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main prints a line of code using puts
+ * main - prints a line of code using puts
  *
- * Return 0
+ * Return: 0
 */
-
-int main (void)
+int main(void)
 {
 	puts("\"Programming ia like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
