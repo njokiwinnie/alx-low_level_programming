843b10d3482ced0690d312b0415ed08de5b997ce
njokiwinnie <winnleykim@gmail.com>
2023-03-16 16:31:30 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print out line with special charactes
+ *
+ * Return: 1
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
