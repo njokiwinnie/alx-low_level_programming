e79aea765bff89b30fd3dc1619cd58c54f98415e
njokiwinnie <winnleykim@gmail.com>
2023-03-16 14:31:07 -0700

    first c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main prints a line of code using puts
+ *
+ * Return 0
+*/
+
+int main (void)
+{
+	puts("\"Programming ia like building a multilingual puzzle\n");
+	return(0);
+}
