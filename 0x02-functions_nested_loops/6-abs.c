658a308d806c29a7a4b951ca461a59eab779a514
root <root@LAPTOP-2GLKAB80>
2023-03-22 01:39:53 +0300

    computes absolute value of interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an interger
+ * @r: the interger to compute
+ * Return: the absolute value of n
+ */
+
+int _abs(int)
+{
+	if (r < 0)
+		_putchar('-')
+	return(r < 0 ? -r : r);
+}
