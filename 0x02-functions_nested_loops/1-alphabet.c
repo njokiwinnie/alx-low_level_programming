9f3da0c5d8cf0412026de03ee4faef20037fd363
root <root@LAPTOP-2GLKAB80>
2023-03-22 02:24:22 +0300

    prints lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * print_alphabet: prints alphabet in lower case
+ *  letter 'a' to 'z': prints letter
+ * `_putchar()` function defined in the header file "unistd.h".
  */
 void print_alphabet(void)
 {
