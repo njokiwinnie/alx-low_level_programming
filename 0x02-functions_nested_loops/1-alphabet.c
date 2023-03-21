79516b3be2f0ef5ab7368c6542f351d6c608746b
root <root@LAPTOP-2GLKAB80>
2023-03-22 02:34:40 +0300

    print alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet: prints alphabet in lower case
- *  letter 'a' to 'z': prints letter
- * `_putchar()` function defined in the header file "unistd.h".
+ * print_alphabet - Prints the lowercase alphabet from 'a' to 'z'.
+ * This function uses a loop to print the lowercase alphabet from 'a' to 'z'
+ * using the `_putchar()` function defined in the header file "unistd.h".
+ * Return: void.
  */
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
 		_putchar(letter);
+	}
 	_putchar('\n');
 }
