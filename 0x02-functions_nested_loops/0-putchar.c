94800ebc76a634746513e6d95486513460155b03
root <root@LAPTOP-2GLKAB80>
2023-03-21 20:12:43 +0300

    putchar statement

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints using _putchar
+ *
+ * Return: 0
+**/
+
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
