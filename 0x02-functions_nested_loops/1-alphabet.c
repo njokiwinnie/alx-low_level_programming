7930ad4ffaf80ed9cdd1d6ce3f800ed015ec51cc
root <root@LAPTOP-2GLKAB80>
2023-03-21 22:18:30 +0300

    Function that prints lowercase alphabet 10 times

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,11 @@
  * print alpphabet iin lower case
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	char alphabet;
+	char letter;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		_putchar(alphabet);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 	_putchar('\n');
-
-	return (0);
 }
