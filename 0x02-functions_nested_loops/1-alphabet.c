152c5cc83493bb79fbb7ef4bf4d97af6b005664e
root <root@LAPTOP-2GLKAB80>
2023-03-21 21:12:49 +0300

    print alphabet using prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check code
+ * print alpphabet iin lower case
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+
+	return (0);
+}
