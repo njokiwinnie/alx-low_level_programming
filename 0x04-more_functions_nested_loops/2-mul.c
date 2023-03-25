f8ab9b7b1bd22430c5f8a08b90a9396ae1655f8e
njokiwinnie <winnleykim@gmail.com>
2023-03-25 11:36:00 +0300

    using mul to multiply two intergers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,10 +3,10 @@
  * mul - function that multiplies two intergers
  * @a: first number
  * @b: second number
- * Return: the product 
+ * Return: the product
  */
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 
 }
