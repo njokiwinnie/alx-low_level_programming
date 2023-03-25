812e86f8b9e6ffc6ba5703cc77a8fe8aa5dccbed
njokiwinnie <winnleykim@gmail.com>
2023-03-25 11:27:51 +0300

    using isupper too check uppercase characters

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two intergers
+ * @a: first interger
+ * @b: second interger
+ * Return: the product 
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+
+}
