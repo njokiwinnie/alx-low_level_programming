5e7f35a3e39b210c046aefd00472c66c1ade3631
njokiwinnie <winnleykim@gmail.com>
2023-04-03 21:36:47 +0300

    use of memcpy to copy memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy - copies memeory area
+ * @dest: destination
+ * @src: source
+ * @n: interger
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+	char *s = src;
+
+	while (n > 0)
+	{
+		*d = *s;
+		++d;
+		++s;
+		--n;
+	}
+	return (dest);
+}
