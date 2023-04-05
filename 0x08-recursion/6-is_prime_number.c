92a5fb8b5e7adc5a08efce5efdf2f64c88ce33b6
njokiwinnie <winnleykim@gmail.com>
2023-04-05 21:50:03 +0300

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,14 +6,22 @@
  */
 int is_prime_number(int n)
 {
-	int i ;
-
-	if (n < 2)
+	if (n <= 1)
 		return (0);
-	for (i = 2; i <= n / 2; i++)
+	else if (n <= 3)
+		return (1);
+	else if (n % 2 == 0 || n % 3 == 0)
+		return (0);
+	else
 	{
-		if (n % i == 0)
-			return (0);
+		int i = 5;
+
+		while (i * i <= n)
+		{
+			if (n % i == 0 || n % (i + 2) == 0)
+				return (0);
+			i += 6;
+		}
+		return (1);
 	}
-	return (1);
 }
