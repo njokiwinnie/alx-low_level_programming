837f92eee1e479bf494b3aefc4275cbada09a222
njokiwinnie <winnleykim@gmail.com>
2023-04-18 00:17:20 +0300

    struct function

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: first field
+ * @age: second field
+ * @owner: third field
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
