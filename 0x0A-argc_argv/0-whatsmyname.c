30d244d4259e3d15f90cffe00f8c2b7b02b6a004
njokiwinnie <winnleykim@gmail.com>
2023-04-18 23:29:12 +0300

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: number of commands
+ * @argv: pointer array
+ * Return: success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
