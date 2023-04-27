65912819cda59247b607d19f41e37f5567db02c6
njokiwinnie <winnleykim@gmail.com>
2023-04-27 20:08:48 +0300

    elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,22 @@
-dbj
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints elements of the the list
+ * @h: pointer to start the list
+ * Return: number of nodes or 0 if NULL
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
