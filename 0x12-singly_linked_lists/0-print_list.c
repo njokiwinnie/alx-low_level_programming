8fffae1aafe95e451e1f56ea5a65f4f387e30283
njokiwinnie <winnleykim@gmail.com>
2023-04-27 21:43:31 +0300

    fix 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,18 @@
  *
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
+		printf("[%u] %s\n",
+			h->str != NULL ? h->len : 0,
+			h->str != NULL ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
-
 	return (count);
 }
