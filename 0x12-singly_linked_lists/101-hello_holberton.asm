d1d98eb1fadf4d7463cc1045302944c69e172e59
njokiwinnie <winnleykim@gmail.com>
2023-04-27 21:40:00 +0300

    advanced

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+; File: hello.asm
+; This program prints "Hello, Holberton" followed by a new line
+
+    global  main
+    extern  printf
+
+    section .data
+msg db 'Hello, Holberton', 0
+
+    section .text
+main:
+    ; Prepare arguments for printf
+    push    rbp
+    mov     rdi, msg        ; Set first argument (format string)
+    xor     eax, eax        ; Set number of vector registers used to 0
+    call    printf          ; Call printf
+
+    ; Clean up stack
+    pop     rbp
+
+    ; Return from main
+    xor     eax, eax        ; Set return value to 0
+    ret
