92a36d49450f1a8bcb48fa8a017e048cd1fc6503
njokiwinnie <winnleykim@gmail.com>
2023-04-29 01:35:11 +0300

    error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+        size_t count;
 
-	for (count = 0; h != NULL; h = h->next, count++)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-	}
+        for (count = 0; h != NULL; h = h->next, count++)
+        {
+                if (h->str == NULL)
+                {
+                        printf("[0] (nil)\n");
+                }
+                else
+                {
+                        printf("[%u] %s\n", h->len, h->str);
+                }
+        }
 
-	return (count);
+        return count;
 }
